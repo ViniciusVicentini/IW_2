2b34a8e485dbc4114bca0e429ca2de546f0602fb
ViniciusVicentini <vinisvicentini@gmail.com>
2026-03-02 08:46:46 -0300

    Add files via upload

---
+++ atividade iw2(Vinicius)/index.js
@@ -0,0 +1,10 @@
+let resposta = confirm("Voce deseja continuar?");
+if (resposta) {
+    alert("Voce escolheu continuar")
+}
+else {
+    alert("Voce cancelou a ação")
+}
+
+let nome = prompt("Qual é o seu nome?");
+alert("Olá, " + nome + "!" );
