60a45d9e6fae30f5cde493dfdcf2f76d95810cb3
ViniciusVicentini <vinisvicentini@gmail.com>
2026-03-02 08:05:44 -0300

    Add files via upload

---
+++ atividade iw2(Vinicius)/exercicios.js
@@ -0,0 +1,46 @@
+var ex1 = "exercicio 1 a)";
+console.log(ex1);
+var a = 10;
+var b = 20;
+console.log(b);
+var b = 5;
+console.log(a, b);
+
+var ex2 = "exercicio 1 b)";
+console.log(ex2);
+var a = 30;
+var b = 20;
+var c = a + b;
+console.log(c);
+var b = 10;
+console.log(b, c);
+var c = a + b;
+console.log(a, b, c)
+
+var ex3 = "exercicio 1 c)";
+console.log(ex3);
+var a = 10;
+var b = 20;
+var c = a;
+var b = c;
+var a = b;
+console.log(a, b, c)
+
+var ex4 = "exercicio 1 d)";
+console.log(ex4);
+
+var a = 10;
+var b = a + 1;
+var a = b + 1;
+var b = a + 1;
+console.log(a);
+var a = b + 1;
+console.log(a, b);
+
+var ex5 = "exercicio 2";
+console.log(ex5);
+var nota1 = 5;
+var nota2 = 7;
+var nota3 = 9;
+var media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+console.log("A média do aluno é ", media);
